65ed2053bdb125ab5db767226a8f327d7cc404b8
pnshiralkar <pnshiralkar@gmail.com>
2020-12-05 02:34:22 +0530

    Added sort table view and sort in admin panel

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from videos.models import Video
 
-admin.site.register(Video)
+
+class VideoAdmin(admin.ModelAdmin):
+    # list_filter = ('title', 'yt_id')
+    sortable_by = ('published_at',)
+    list_display = ('yt_id', 'title', 'published_at')
+
+
+admin.site.register(Video, VideoAdmin)
